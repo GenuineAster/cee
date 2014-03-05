6d8eca52477bf20f53e64f78139721ac91dd4357
Mischa-Alff <mischa.alff@gmail.com>
2014-03-05 09:59:00 +0100

    Fixed bug where code would return RF

--- plugins/g++/__init__.py
+++ plugins/g++/__init__.py
@@ -18,8 +18,7 @@ class MiniSandbox(SandboxPolicy, Sandbox):
 	sc_table = None
 	# white list of essential linux syscalls for statically-linked C programs
 	sc_safe = dict(i686=set([0, 3, 4, 19, 45, 54, 90, 91, 122, 125, 140,
-		163, 192, 197, 224, 243, 252, ]), x86_64=set([0, 1, 5, 8, 9, 10,
-        11, 12, 16, 25, 63, 158, 219, 231, ]#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 219, 231]
+		163, 192, 197, 224, 243, 252, ]), x86_64=set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 219, 231]
 ), )
 	# result code translation table
 	result_name = dict((getattr(Sandbox, 'S_RESULT_%s' % r), r) for r in
@@ -104,7 +103,7 @@ class Plugin(BasePlugin, object):
 		program_output_raw  = ""
 		message_string = ""
 
-		output = open("/tmp/cee_output", "w")
+		output = open("/tmp/cee_output", "w+")
 
 		cookbook = {
 			'args': filename,               # targeted program
@@ -139,7 +138,13 @@ class Plugin(BasePlugin, object):
 
 			message_string = to_bytes(message_string)
 
-			if program_output_data.get("result", False) is not "TL":
+			print(program_output_data.get("result", False))
+
+			if program_output_data.get("result", False) is "TL":
+				message_string = "<killed>"
+			elif program_output_data.get("result", False) is "RF":
+				message_string = "<killed> ( restricted function used )"
+			else:
 				if program_output[0]:
 
 					if len(program_output) > 1:
@@ -151,8 +156,6 @@ class Plugin(BasePlugin, object):
 
 				else:
 					message_string = "<no output> ( return value was %d ) " % program_output_data.get("exitcode", -1)
-			else:
-				message_string = "<killed>"
 
 			return message_string
 
