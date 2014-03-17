a8d5686cdad666e222c271a3b1a832d957611855
Mischa-Alff <mischa.alff@gmail.com>
2014-03-17 23:52:13 +0100

    Created Malbolge plugin
    what have i done

---
+++ plugins/malbolge/__init__.py
@@ -0,0 +1,36 @@
+import plugins.InterpreterPlugin
+
+
+class Plugin(plugins.InterpreterPlugin.InterpreterPlugin, object):
+
+    name = None
+    author = None
+    description = None
+    connection = None
+
+    def __init__(self, **kwargs):
+        self.name = "malbolge"
+        self.author = "Mischa-Alff"
+        self.description = "A Malbolge evaluation plugin."
+
+        self.interpreter_command = ["/usr/bin/python2", "/usr/bin/malbolge.py"]
+
+        super(Plugin, self).__init__(**kwargs)
+
+        self.commands.append(
+            plugins.BasePlugin.Command(
+                self.snippet, ["%%prefix%%malbolge"], [""],
+                {
+                    "lang_extension": "mbs"
+                }
+            )
+        )
+
+        self.commands.append(
+            plugins.BasePlugin.Command(
+                self.snippet, ["%%prefix%%mbs"], [""],
+                {
+                    "lang_extension": "mbs"
+                }
+            )
+        )
