16c5d961951a96dd48eb04562bfe75ce08bc5124
Mischa-Alff <mischa.alff@gmail.com>
2014-03-17 22:16:46 +0100

    Created php plugin

---
+++ plugins/php/__init__.py
@@ -0,0 +1,27 @@
+import plugins.InterpreterPlugin
+
+
+class Plugin(plugins.InterpreterPlugin.InterpreterPlugin, object):
+
+    name = None
+    author = None
+    description = None
+    connection = None
+
+    def __init__(self, **kwargs):
+        self.name = "php"
+        self.author = "Mischa-Alff"
+        self.description = "A PHP evaluation plugin."
+
+        self.interpreter_command = ["/usr/bin/php"]
+
+        super(Plugin, self).__init__(**kwargs)
+
+        self.commands.append(
+            plugins.BasePlugin.Command(
+                self.snippet, ["php", ""], [""],
+                {
+                    "lang_extension": "php"
+                }
+            )
+        )
