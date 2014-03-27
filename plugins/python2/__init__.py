c5f93d5e325fb8e1bf27766cd5aa71fde5f0081f
Mischa-Alff <mischa.alff@gmail.com>
2014-03-27 12:20:42 +0100

    Gave Python2 a better traceback parser

--- plugins/python2/__init__.py
+++ plugins/python2/__init__.py
@@ -12,6 +12,26 @@ class Plugin(plugins.InterpreterPlugin.InterpreterPlugin, object):
         data["command"] = data["command"].replace(";;", "\n")
         return self.snippet(data, extra_args)
 
+    def parse_output(self, message_string, program_output):
+        found = [False, False]
+        for line in program_output:
+            if "traceback" in line.lower():
+                print("found!")
+                found = [True, program_output.index(line)]
+                print found
+
+        if found[0]:
+            print("penis")
+            found[1] += 1
+            while found[1] < len(program_output):
+                if program_output[found[1]][0] != " ":
+                    message_string = program_output[found[1]]
+                    break
+                found[1] += 1
+
+        message_string = self.strip_output(message_string, program_output)
+        return message_string
+
     def __init__(self, **kwargs):
         self.name = "python"
         self.author = "Mischa-Alff"
