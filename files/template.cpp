6f8bcfea11ac0cd0a38e8813ef0cca455c36e6fd
Mischa Aster Alff <mischa.alff@gmail.com>
2015-06-19 18:22:40 +0200

    Added binary stream modifier in C++ template

--- files/template.cpp
+++ files/template.cpp
@@ -51,7 +51,70 @@
 #include <future>
 #include <condition_variable>
 #include <utility>
+#include <experimental/optional>
+//#include <experimental/memory_resource>
+//#include <experimental/algorithm>
+//#include <experimental/numeric>
+//#include <experimental/execution_policy>
+//#include <experimental/>
+//#define GLM_SWIZZLE
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+
+
 
 using namespace std;
+using namespace glm;
 
 #define RANGE(r) std::begin(r), std::end(r)
+
+namespace cee {
+struct bin_proxy {
+    explicit bin_proxy(std::ostream & os):os(os){}
+
+    template<typename Rhs>
+    friend std::ostream &operator<<(bin_proxy const &b, Rhs const &rhs) {
+        return b.os << rhs;
+    }
+
+    friend std::ostream &operator<<(bin_proxy const &b, uint8_t const &rhs) {
+        return b.os << bitset<8>(rhs);
+    }
+    friend std::ostream &operator<<(bin_proxy const &b, int8_t const &rhs) {
+        return b.os << bitset<8>(rhs);
+    }
+
+    friend std::ostream &operator<<(bin_proxy const &b, uint16_t const &rhs) {
+        return b.os << bitset<16>(rhs);
+    }
+    friend std::ostream &operator<<(bin_proxy const &b, int16_t const &rhs) {
+        return b.os << bitset<16>(rhs);
+    }
+
+    friend std::ostream &operator<<(bin_proxy const &b, uint32_t const &rhs) {
+        return b.os << bitset<32>(rhs);
+    }
+    friend std::ostream &operator<<(bin_proxy const &b, int32_t const &rhs) {
+        return b.os << bitset<32>(rhs);
+    }
+
+    friend std::ostream &operator<<(bin_proxy const &b, uint64_t const &rhs) {
+        return b.os << bitset<64>(rhs);
+    }
+    friend std::ostream &operator<<(bin_proxy const &b, int64_t const &rhs) {
+        return b.os << bitset<64>(rhs);
+    }
+
+
+
+
+private:
+    std::ostream &os;
+};
+
+struct bin_creator { } bin;
+bin_proxy operator<<(std::ostream &os, bin_creator) {
+    return bin_proxy(os);
+}
+}
